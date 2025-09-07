4e2e6b8b9aa5264d6a2ebd02764d3960bd6cfc11
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-07 17:44:14 +0300

    Задание 2

--- Program.cs
+++ Program.cs
@@ -12,7 +12,15 @@
             //Console.WriteLine(a * b);
             //Console.WriteLine(a / b);
 
-           
+            //Задание 2 
+            //string name = Console.ReadLine();
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //string language = Console.ReadLine();
+            //Console.WriteLine($"Привет, {name}!");
+            //Console.WriteLine($"Тебе {age} лет.");
+            //Console.WriteLine($"Твой любимый язык программирования — {language}.");
+
+            
         }
     }
 }
