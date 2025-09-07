9e31743929316ad9becd7e5a8fa5de44e74bfbe7
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-07 17:45:05 +0300

    Задание 4

--- Program.cs
+++ Program.cs
@@ -30,7 +30,38 @@
 
             //Console.WriteLine(f);
 
-           
+            //Задание 4 
+
+            //Console.Write("Введите своё имя: ");
+            //string name = Console.ReadLine();
+            //Console.Write("Введите свой возраст: ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите ваш город: ");
+            //string citi = Console.ReadLine();
+            //Console.Write("Введите ваше любимое число: ");
+            //double number = Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Введите температру в С: ");
+            //double C = Convert.ToDouble(Console.ReadLine());
+            //double F = ((C * 9) / 5) + 32;
+            //double cvad = Math.Pow(number, 2);
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("=== ПЕРСАНАЛЬНЫЕ ДАННЫЕ ===");
+            //Console.WriteLine();
+            //Console.WriteLine($"Привет,{name}!");
+            //Console.WriteLine($"Тебе {age} лет.");
+            //Console.WriteLine($"В следующем гоу тебе будет {age +1}.");
+            //Console.WriteLine();
+            //Console.WriteLine($"Ты живёшь в городе {citi}.");
+            //Console.WriteLine($"Твоё любимое число: {number} (в квадрате: {cvad}) ");
+            //Console.WriteLine();
+            //Console.WriteLine("Температура за окном:");
+            //Console.WriteLine($" - Температура в Цельсиях: {C}C");
+            //Console.WriteLine($" - Температура в Фаренгейтах: {F}F");
+            //Console.WriteLine();
+
+            //Console.WriteLine("=== КОНЕЦ ПРОФИЛЯ ===");
         }
     }
 }
