44e96a76b0199a6e106ac1f5d41661a04a987786
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-07 17:40:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab1_Homework_Alekseev_ISP_232
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
